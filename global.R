5d3a880a62cb4429d12dd532c3eed06b34722ef9
yin-huamin <yinhm17@126.com>
2022-10-20 22:21:53 +0800

    Update global.R

--- global.R
+++ global.R
@@ -1,14 +1,16 @@
+#Sys.setlocale("LC_ALL", "English")
 library(shiny)
+library(rsconnect)
 library(ggplot2)
 library(data.table)
 library(dplyr)
 library(ggprism)
-library(magrittr)
+#library(magrittr)
 library(ggrepel)
 library(stringr)
 
-
-load("data/diffres.rda")
+# saveRDS(diffres,"data/diffres.rds")
+diffres <- data.table::fread("data/diffres.txt")
 
 source("func/plot_volcano.R")
 
